38fd91b4f465166e5bce27d8f7d307f0eb292f7d
Vieru Daniel <daniel_vieru1@yahoo.com>
2023-08-07 16:28:34 +0300

    all part 5

---
+++ basic/controllers/MyArticleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+//article/hello-world
+//article->
+class MyArticleController extends \yii\web\Controller
+
+{
+    public function actionHelloSon()
+    {
+         echo "Hello World";
+    }
+
+}
